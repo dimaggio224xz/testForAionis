94411bddc2a1a6ff542408ff3aedb390d4506e89
Dimaggio224 <dima224xz@gmail.com>
2020-09-07 19:41:12 +0300

    123

--- back/server.js
+++ back/server.js
@@ -1,11 +1,228 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+
+
+
+
+
+function createId() {
+    return '' +  Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
+}
+
+function getAllNotes() {
+    return ( JSON.parse(fs.readFileSync('./database.json', 'utf-8')) ).notes;
+}
+
+function getNoteById(_id) {
+    _id = ''+_id;
+    const arr = getAllNotes()
+    if (arr.length === 0) {
+        return null;
+    }
+    const resault = arr.filter((item)=> item && item._id && item._id === _id);
+    return resault.length === 0 ? null : resault[0];
+}
+
+function deleteNote(_id) {
+    _id = ''+_id;
+    const arr = getAllNotes();
+
+    if (arr.length === 0) {
+        return null;
+    }
+
+    let resultArr =[];
+    for (let item of arr) {
+        if(item._id && item._id !== _id) {
+            resultArr.push(item);
+        }
+    }
+    if (resultArr.length !== 0 && arr.length > resultArr.length) {
+        fs.writeFileSync('./database.json', JSON.stringify({notes: resultArr}, null, 4));
+        return true;
+    }
+    return null;
+}
+
+function createNote(title, text, date) {
+    const obj ={
+        title,
+        text,
+        date,
+        _id: createId(),
+        completed: false
+    }
+    const arr = getAllNotes()
+    arr.unshift(obj);
+    
+    fs.writeFileSync('./database.json', JSON.stringify({notes: arr}, null, 4));
+    return true;
+}
+
+function updateNote(_id, title, text){
+    _id = ''+_id;
+    const arr = getAllNotes()
+    let resultArr = [];
+
+    for(let item of arr) {
+        if(item._id === _id) {
+            resultArr.push({
+                _id,
+                title,
+                text,
+                date: item.date,
+                completed: item.completed
+            })
+        }
+        else {
+            resultArr.push(item);
+        }
+    }
+
+    fs.writeFileSync('./database.json', JSON.stringify({notes: resultArr}, null, 4));
+    return true;
+}
+
+function completedOrNot(_id, status) {
+    _id = ''+_id;
+    const arr = getAllNotes();
+    let resultArr = [];
+
+    for(let item of arr) {
+        if(item._id === _id) {
+            resultArr.push({
+                _id,
+                title: item.title,
+                text: item.text,
+                date: item.date,
+                completed: status
+            })
+        }
+        else {
+            resultArr.push(item);
+        }
+    }
+
+    fs.writeFileSync('./database.json', JSON.stringify({notes: resultArr}, null, 4));
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+app.get('/get-all', function (req, res) {
+    res.end(JSON.stringify(getAllNotes()))
+});
+
+
+app.get('/get/:id', function (req, res) {
+    if(req.params.id) {
+        const note = getNoteById(req.params.id);
+        if(note) {
+            res.end(JSON.stringify({note}))
+        } else {
+            res.end(JSON.stringify({msg: 'ERROR'}))
+        }
+    } 
+    else {
+        res.end(JSON.stringify({msg: 'ERROR'}))
+    }
+});
+
+
+app.post('/create-note', function (req, res) {
+    const {title, text, date} = req.body;
+
+    if (!title || !text || !date) {
+        res.end(JSON.stringify({msg: 'ERROR'}))
+    }
+
+    const answer = createNote(title, text, date);
+
+    if (answer) {
+        res.end(JSON.stringify({msg: 'SAVE'}))
+    } else {
+        res.end(JSON.stringify({msg: 'ERROR'}))
+    }
+});
+
+app.post('/update-note', function (req, res) {
+    const {_id, title, text} = req.body;
+
+    if (!_id || !title || !text) {
+        res.end(JSON.stringify({msg: 'ERROR'}))
+    }
+
+    const answer = updateNote(_id, title, text);
+
+    if (answer) {
+        res.end(JSON.stringify({msg: 'SAVE'}))
+    } else {
+        res.end(JSON.stringify({msg: 'ERROR'}))
+    }
+
+});
+
+
+app.put('/completed/:id', function (req, res) {
+    if(req.params.id) {
+        let answer = completedOrNot(req.params.id, true)
+
+        if (answer) {
+            res.end(JSON.stringify({msg: 'SAVE'}))
+        } else {
+            res.end(JSON.stringify({msg: 'ERROR'}))
+        }
+    } 
+    else {
+        res.end(JSON.stringify({msg: 'ERROR'}))
+    }
+});
+
+
+app.put('/un-completed/:id', function (req, res) {
+    if(req.params.id) {
+        let answer = completedOrNot(req.params.id, false)
+
+        if (answer) {
+            res.end(JSON.stringify({msg: 'SAVE'}))
+        } else {
+            res.end(JSON.stringify({msg: 'ERROR'}))
+        }
+    } 
+    else {
+        res.end(JSON.stringify({msg: 'ERROR'}))
+    }
+});
+
+
+app.delete('/delete/:id', function (req, res) {
+    if(req.params.id) {
+        const answer = deleteNote(req.params.id);
+        if (answer) {
+            res.end(JSON.stringify({msg: 'DELETED'}))
+        }
+        else {
+            res.end(JSON.stringify({msg: 'ERROR'}))
+        }
+    } 
+    else {
+        res.end(JSON.stringify({msg: 'ERROR'}))
+    }
+});
 
 
 
--- front/src/components/getServerDatas/index.js
+++ front/src/components/getServerDatas/index.js
@@ -1,3 +1,23 @@
-export default {
-    
+export default 
+{
+    getAllNotes: async () =>{
+        const res = await fetch("/get-all");
+        if (!res.ok) throw new Error(`COULD_NOT_GET_DATA`);
+        else return await res.json();
+    }
 }
+
+
+// let makeCheckIn = async (nick, email, password) => {
+//     const res = await fetch("/users/new", {
+//         headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//         },
+//         method: "POST",
+//         body: JSON.stringify({nick, email, password})
+//     })
+    
+//     if (!res.ok) throw new Error(`COULD_NOT_GET_DATA`);
+//     else return await res.json();
+// }
